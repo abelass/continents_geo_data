abe8a61422dda162fc1dda31868eada3eafe485a
Rainer Müller <rainer@websimple.be>
2018-10-14 10:42:12 +0200

    fabrique

---
+++ lang/paquet-continents_geo_data_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'continents_geo_data_description' => 'Ajoute des donnés géométriques au plugin continents',
+	'continents_geo_data_nom' => 'Donnés géométriques continents',
+	'continents_geo_data_slogan' => 'Des donnés géométriques pour continents',
+);
