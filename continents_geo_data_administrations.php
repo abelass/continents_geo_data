f87d063893ab7662dc15a30db548c93004f7d494
abelass <rainer@websimple.be>
2018-10-22 14:26:02 +0200

    points par défaut si pas polygons
    renonce à antarctica, donnés corrupt

--- continents_geo_data_administrations.php
+++ continents_geo_data_administrations.php
@@ -8,11 +8,9 @@
  * @licence    GNU/GPL
  * @package    SPIP\Continents_geo_data\Installation
  */
-
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
-
 
 /**
  * Fonction d'installation et de mise à jour du plugin Donnés géométriques continents.
@@ -25,11 +23,11 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * - mettre à jour la structure SQL
  *
  * @param string $nom_meta_base_version
- *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ *        	Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
  * @param string $version_cible
- *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ *        	Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
  * @return void
-**/
+ */
 function continents_geo_data_upgrade($nom_meta_base_version, $version_cible) {
 	include_spip('base/upgrade');
 	include_spip('base/peupler_continents_geo');
@@ -37,18 +35,37 @@ function continents_geo_data_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
 	$maj['create'] = array(
-		array('sql_alter', 'TABLE spip_continents ADD geo GEOMETRY AFTER longitude'),
+		// cr´e la table
+		array(
+			'sql_alter',
+			'TABLE spip_continents ADD geo GEOMETRY AFTER longitude'
+		),
+		// Remplir la table avec les points
+		array(
+			'sql_update',
+			'spip_continents',
+			array(
+				'geo' => "GeomFromText(CONCAT('POINT(',longitude,' ',latitude,')'))"
+			)
+		),
+		// remplacer le points par polygons s'ils existent
 		array(
 			'peupler_continents_geo'
 		),
-		array('sql_alter', 'TABLE spip_continents CHANGE COLUMN geo geo GEOMETRY NOT NULL'),
-		array('sql_alter', 'TABLE spip_continents ADD SPATIAL INDEX (geo)'),
+	// mettre la colonne geo à NOT NULL pour permettre l'indexation
+		array(
+			'sql_alter',
+			'TABLE spip_continents CHANGE COLUMN geo geo GEOMETRY NOT NULL'
+		),
+		// Indexe la colonne geo
+		array(
+			'sql_alter',
+			'TABLE spip_continents ADD SPATIAL INDEX (geo)'
+		)
 	);
-
 
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
-
 
 /**
  * Fonction de désinstallation du plugin Donnés géométriques continents.
@@ -59,15 +76,13 @@ function continents_geo_data_upgrade($nom_meta_base_version, $version_cible) {
  * - supprimer les tables et les champs créés par le plugin.
  *
  * @param string $nom_meta_base_version
- *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ *        	Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
  * @return void
-**/
+ */
 function continents_geo_data_vider_tables($nom_meta_base_version) {
-	# quelques exemples
-	# (que vous pouvez supprimer !)
-	# sql_drop_table('spip_xx');
-	# sql_drop_table('spip_xx_liens');
-
-
+	// quelques exemples
+	// (que vous pouvez supprimer !)
+	// sql_drop_table('spip_xx');
+	// sql_drop_table('spip_xx_liens');
 	effacer_meta($nom_meta_base_version);
 }
